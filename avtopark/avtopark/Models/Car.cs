b1e72566ae311e17176a4d98a544c44c639cff11
stivan301 <stef_321@abv.bg>
2020-03-01 11:52:14 +0200

    Zapis AvtoClients

---
+++ avtopark/avtopark/Models/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace avtopark.Models
+{
+    public class Car
+    {   [Key]
+        public string Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Model { get; set; }
+        [Required]
+        [MaxLength(10)]
+        public string Color { get; set; }
+        public DateTime? YearMaking { get; set; }
+        [Required]
+        public double PriceNaem { get; set; }
+        //Навигационно свойство
+        public Client Client { get; set; }
+    }
+}
---
+++ avtopark/avtopark/Program.cs
@@ -0,0 +1,34 @@
+﻿using avtopark.Models;
+using System;
+
+namespace avtopark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new AvtosalovDbContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            Car cars1 = new Car();
+            cars1.Model = "BMW";
+            cars1.Color = "Black";
+            cars1.YearMaking = new DateTime(2010, 02, 28);
+            cars1.PriceNaem = 124;
+            cars1.Id = "1000";
+            context.Cars.Add(cars1);
+            //........
+            Car cars2 = new Car();
+            cars2.Model = "Moskvich";
+            cars2.Color = "Green";
+            cars2.YearMaking = new DateTime(1978, 02, 28);
+            cars2.PriceNaem = 12.36;
+            cars2.Id = "7777";
+            context.Cars.Add(cars2);
+            context.SaveChanges();
+            //.....
+            
+        }
+    }
+}
