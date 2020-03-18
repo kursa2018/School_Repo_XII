df17287f3330971bc421cda14e22e047d7163df8
stivan301 <stef_321@abv.bg>
2020-03-18 20:06:50 +0200

    нова промяна

--- HospitalDb/HospitalDb/Connection.cs
+++ HospitalDb/HospitalDb/Connection.cs
@@ -6,6 +6,6 @@ namespace HospitalDb
 {
     public static class Connection
     {
-        public const string ConnectionString = @"Server=DESKTOP-3D8F6H5\SQLEXPRESS;Database=HospitalDb;Integrated Security=true";
+        public const string ConnectionString = @"Server=DESKTOP-MLDU4BM\SQLEXPRESS;Database=HospitalDb;Integrated Security=true";
     }
 }
