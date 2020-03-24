f6485c94ff8d02fe267ddc23e121e878e7a4e24f
stef_321 <stef_321@abv.bg>
2020-03-24 20:53:55 +0200

    Начални данни за докторите в болницата

--- HospitalDb/HospitalDb/Program.cs
+++ HospitalDb/HospitalDb/Program.cs
@@ -1,4 +1,5 @@
-﻿using HospitalDb.Presentation;
+﻿using HospitalDb.Models;
+using HospitalDb.Presentation;
 using System;
 
 namespace HospitalDb
@@ -11,6 +12,40 @@ namespace HospitalDb
             //Изтриваме и създаваме BD
             context.Database.EnsureDeleted();
             context.Database.EnsureCreated();
+            Doctor d1 = new Doctor();
+            d1.Cabinet = 202;
+            d1.Name = "Статко Статков";
+            d1.Specialnost = "уши нос и гърло";
+            context.Add(d1);
+            //................
+            Doctor d2 = new Doctor();
+            d2.Cabinet = 232;
+            d2.Name = "Петър Гроздарев";
+            d2.Specialnost = "офталмолог";
+            context.Add(d2);
+            context.SaveChanges();
+            //................
+            Doctor d3 = new Doctor();
+            d3.Cabinet = 569;
+            d3.Name = "Халил Доспатлийски";
+            d3.Specialnost = "гинеколог";
+            context.Add(d3);
+            context.SaveChanges();
+            //................
+            Doctor d4 = new Doctor();
+            d4.Cabinet = 500;
+            d4.Name = "Стефан Папанов";
+            d4.Specialnost = "психиатър";
+            context.Add(d4);
+            context.SaveChanges();
+            //..................
+            Doctor d5 = new Doctor();
+            d5.Cabinet = 400;
+            d5.Name = "Алиш Алишов";
+            d5.Specialnost = "уши нос и гърло";
+            context.Add(d5);
+            context.SaveChanges();
+            
             Display displayDoctors = new Display();
         }
     }
