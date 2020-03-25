68a7fb2675508ff77863b90ab3709d3a43460022
stef_321 <stef_321@abv.bg>
2020-03-25 21:55:47 +0200

    Презаписване на проекта Коли под наем

--- ColaPodNaem/ColaPodNaem/Connection.cs
+++ ColaPodNaem/ColaPodNaem/Connection.cs
@@ -4,11 +4,8 @@ using System.Text;
 
 namespace ColaPodNaem
 {
-    public static class Connection
+   public static class Connection
     {
-        // public const string ConnectionString = @"Server=.;Database=avtoBd;Integrated Security=true";
-
-        //Alishov PC
-        public const string ConnectionString = @"Server=DESKTOP-RG79DFT\SQLEXPRESS;Database=avtoBd;Integrated Security=true";
+        public const string ConnectionString = @"Server=DESKTOP\SQLEXPRESS;Database=avtoBd;Integrated Security=true";
     }
 }
