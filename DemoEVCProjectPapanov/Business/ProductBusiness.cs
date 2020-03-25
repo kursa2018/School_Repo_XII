f2486d79012d4144b7da337dfd6e1ff4989ca501
stef_321 <stef_321@abv.bg>
2020-03-25 10:41:14 +0200

    Добавих некакъв клас дето не стана

---
+++ DemoEVCProjectPapanov/Business/ProductBusiness.cs
@@ -0,0 +1,65 @@
+﻿using Data.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class ProductBusiness
+    {
+        private ProductContext context;
+        //Извлича всички продукти
+        public List<Product>GetAll()
+        {
+            using (context = new ProductContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+        //Процедура Get()
+        public Product Get(int Id)
+        {
+            using (context = new ProductContext())
+            {
+                return context.Products.Find(Id);
+            }
+        }
+        //Добавяне на продукт Add()
+        public void Add(Product product)
+        {
+            using (context = new ProductContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+        //Презаписване на продукт
+        public void Update(Product product)
+        {
+            using (context = new ProductContext())
+            {
+                var item = context.Products.Find(product.Id);
+                if (item!=null)
+                {
+                    context.Entry(item).CurrentValues.SetValues(product);
+                    context.SaveChanges();
+                }
+            }
+        }
+        //Процедура изтриване Delete()
+        public void Delete(int Id)
+        {
+            using (context = new ProductContext())
+            {
+                var item = context.Products.Find(Id);
+                if (item!=null)
+                {
+                    context.Products.Remove(item);
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ DemoEVCProjectPapanov/Business/ProductContext.cs
@@ -0,0 +1,23 @@
+﻿using Data.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public class ProductContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(Connection.ConnectionString);
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
