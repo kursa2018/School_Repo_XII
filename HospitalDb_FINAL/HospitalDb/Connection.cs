862d60a0c83cfd514d97784b6c96fb6b80b5119e
spapanov@gmail.com <spapanov@gmail.com>
2020-03-29 12:13:34 +0300

    Добавяне на готов проект

---
+++ HospitalDb_FINAL/HospitalDb/Connection.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HospitalDb
+{
+    public static class Connection
+    {
+        public const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=HospitalDb;Integrated Security=true";
+    }
+}
