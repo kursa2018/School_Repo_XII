31985314aba19845b88f62f496b62c1e983ff621
Alish Alishov <48414358+aalishov@users.noreply.github.com>
2020-03-31 13:50:38 +0300

    Add new menu element

--- ColaPodNaem_FINAL/ColaPodNaem/Connection.cs
+++ ColaPodNaem_FINAL/ColaPodNaem/Connection.cs
@@ -6,6 +6,6 @@ namespace ColaPodNaem
 {
    public static class Connection
     {
-        public const string ConnectionString = @"Server=DESKTOP-Q7K8NEA\SQLEXPRESS;Database=avtoBd;Integrated Security=true";
+        public const string ConnectionString = @"Server=(localdb)\mssqllocaldb;Database=avtoBd;Trusted_Connection=true;ConnectRetryCount=0";
     }
 }
